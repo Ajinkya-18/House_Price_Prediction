713e0bab3b1596b4fe6b279a3b1698d1dc9d4d9e
Ajinkya Tamhankar <ajinkya.tamhankar18@gmail.com>
2025-07-16 21:45:13 +0530

    Updated feature selection and model training pipeline.

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,16 @@
 def assert_path(file_path:str):
-    pass
+    import os
+    from pathlib import Path
+
+    corrected_file_path = file_path.replace('\\', '/')
+    cwd = os.getcwd()
+    full_file_path = os.path.join(cwd, corrected_file_path)
+
+    if os.path.exists(full_file_path) and (full_file_path.endswith('.csv') or full_file_path.endswith('.joblib')):
+        return full_file_path
+    
+    else:
+        raise ValueError('Invalid file path or ')
 
 #-----------------------------------------------------------------------------------------------------------------------
 
